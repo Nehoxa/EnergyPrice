38e540248dc73bee4260c9b77fcb42c13ce31ff6
Nehoxa <loirejulien@outlook.fr>
2023-01-10 12:22:42 +0100

    Add Vue Js 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
 import './bootstrap';
+import CartComponent from './components/CartComponent.vue'
+
+
+const app = createApp();
+
+app
+  .component('CartComponent', CartComponent);
+
+app
+  .mount('#app');
